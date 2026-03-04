efd436ddd34f424152e41ea559e3195496ea1d7c
alguemdoido <forcaextm01@gmail.com>
2026-03-04 01:48:21 -0300

    feat: chamar registerWebhook() da EFI no boot do servidor

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const { initDB } = require('./db/schema');
 const { startBot } = require('./bot/index');
 const { startServer } = require('./web/server');
 const { startScheduler } = require('./services/scheduler');
+const { registerWebhook } = require('./services/efi');
 
 async function main() {
   await initDB();
   await startBot();
   startServer();
   startScheduler();
+  // Registra o webhook da EFI automaticamente no boot
+  await registerWebhook();
   console.log('\u2705 Bot, servidor e scheduler iniciados');
 }
 
